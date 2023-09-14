abdf8d0a0781b724137980398f8f5c967e6490b2
fizah <141141649+nhfizhn@users.noreply.github.com>
2023-09-14 20:52:29 +0800

    Update task1
    - added 3 more columns to the tibble
    - changed to using `for` loop instead

--- 01-gen_collatz.R
+++ 01-gen_collatz.R
@@ -2,7 +2,7 @@
 
 library(tibble)
 library(dplyr)
-library(purrr)
+library(tidyverse)
 
 gen_collatz <- function(n){
   
@@ -27,5 +27,20 @@ gen_collatz <- function(n){
   return(collatz_seq)
 }
 
-# Create an empty tibble
-collatz_df <- tibble(start = 1:10000, seq = map(start, gen_collatz))
+# Create an empty tibble with 5 columns
+collatz_df <- tibble(start = integer(0), 
+                     seq = list(), 
+                     length = double(0), 
+                     parity = character(0), 
+                     max_val = double(0))
+
+# Apply the function to integers from 1 to 10,000
+for (i in 1:10000) {
+  collatz_seq <- gen_collatz(i)
+  collatz_df <- collatz_df %>%
+    add_row(start = i, 
+            seq = list(collatz_seq), 
+            length = length(collatz_seq), 
+            parity = ifelse(i %% 2 == 0, "Even", "Odd"), 
+            max_val = max(collatz_seq))
+}
