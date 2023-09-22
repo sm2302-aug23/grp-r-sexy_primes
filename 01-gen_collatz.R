64beaf09784a0046467648198e4ba2824acdf405
nicholas <21b2047@ubd.edu.bn>
2023-09-22 13:36:36 +0800

    Revert "Changed collatz_df to include backtracking"
    This reverts commit 3b0353bc3c2e435ad8c85e81d7b18d3bfe5a915c.

--- 01-gen_collatz.R
+++ 01-gen_collatz.R
@@ -14,49 +14,33 @@ gen_collatz <- function(n){
   # Initial vector for storing collatz sequence
   collatz_seq <- c(n)
   
-  #Initialization for backtracking check
-  decrease <- FALSE
-  backtracking <- FALSE
-  
   # Generate the sequence until it reaches 1
   while(n != 1){
-    if(n < collatz_seq[1]){
-      decrease <- TRUE
-    }
-    
     if(n %% 2 == 0){
       n <- n / 2
     } else{
       n <- 3 * n + 1
     }
     collatz_seq <- c(collatz_seq, n)
-    
-    #Test for backtracking
-    if(decrease & n > collatz_seq[1]){
-      backtracking <- TRUE
-    }
   }
   
-  return(list(collatz_seq,backtracking))
+  return(collatz_seq)
 }
 
-# Create an empty tibble with 6 columns
+# Create an empty tibble with 5 columns
 collatz_df <- tibble(start = integer(0), 
                      seq = list(), 
                      length = double(0), 
                      parity = character(0), 
-                     max_val = double(0),
-                     backtracking = logical(0))
+                     max_val = double(0))
 
 # Apply the function to integers from 1 to 10,000
 for (i in 1:10000) {
-  collatz_seq <- gen_collatz(i)[[1]]
-  backtracking <- gen_collatz(i)[[2]]
+  collatz_seq <- gen_collatz(i)
   collatz_df <- collatz_df %>%
     add_row(start = i, 
             seq = list(collatz_seq), 
             length = length(collatz_seq), 
             parity = ifelse(i %% 2 == 0, "Even", "Odd"), 
-            max_val = max(collatz_seq),
-            backtracking = backtracking)
+            max_val = max(collatz_seq))
 }
