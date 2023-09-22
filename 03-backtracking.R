49df7efa2bdce4c27ba371375c3b5ad0bf8da682
nicholas <21b2047@ubd.edu.bn>
2023-09-22 16:00:53 +0800

    complete task 3
    renamed the function to better represent what it does
    function now checks the maximum value after a backtrack is started

--- 03-backtracking.R
+++ 03-backtracking.R
@@ -1,7 +1,11 @@
-is_backtracking <- function(seq, start){
+#Function to check if a sequence is backtracking
+backtracking_stats <- function(seq, start){
+  #Initializing
   decrease <- FALSE
   backtracking <- FALSE
   above_count <- 0
+  backtrack_started <- FALSE
+  max_after_backtrack <- NA
   
   for(i in seq){
     if(i < start){
@@ -11,17 +15,48 @@ is_backtracking <- function(seq, start){
     if(decrease && i > start){
       backtracking <- TRUE
       above_count <- above_count + 1
+      
+      if(!backtrack_started){
+        backtrack_started <- TRUE
+        max_after_backtrack <- i
+      }
+      
+      if(backtrack_started && i > max_after_backtrack){
+        max_after_backtrack <-i
+      }
     }
   }
-  return(list(backtracking, above_count))
+  return(list(backtracking, above_count, max_after_backtrack))
 }
 
+#Making a new data frame with more info regarding backtracking
 backtracks_df <- collatz_df %>%
   rowwise() %>%
-  mutate(backtracking = is_backtracking(seq,start)[1], above_count = is_backtracking(seq,start)[2]) %>%
+  mutate(backtracking = backtracking_stats(seq,start)[1],
+         above_count = backtracking_stats(seq,start)[2],
+         max_after_backtrack = backtracking_stats(seq,start)[3]) %>%
   filter(., backtracking == TRUE) %>%
   subset(., select = -backtracking)
 
-mode_backtrack <- mode(backtracks_df$above_count)
-  
+#Taken from Ken Williams https://stackoverflow.com/questions/2547402/how-to-find-the-statistical-mode
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+#Most frequently occurring number of times they go above their starting integer 
+mode_backtrack <- as.integer(Mode(backtracks_df$above_count))
+
+#The maximum value reached after the first backtrack for these sequences
+max_after_backtrack <- unlist(backtracks_df$max_after_backtrack)
+
+#Frequency counts for even and odd backtracking integers
+#Even
+n_even <- filter(.data = backtracks_df, parity == "Even") %>%
+           nrow(.)
+
+#Odd
+n_odd <- filter(.data = backtracks_df, parity == "Odd") %>%
+          nrow(.)
+
+even_odd_backtrack <- c(n_even, n_odd)
   
