99d64eb8f5428413eb3412c50b0015efcd6efbe5
AzharAmiin <20b2124@ubd.edu.bn>
2023-09-24 21:11:07 +0800

    Update 04-plots.R

--- 04-plots.R
+++ 04-plots.R
@@ -1,4 +1,4 @@
-#1 plot
+#1 plot--------------------------------------------------------------------------
 
 plot1 <- ggplot(collatz_df, aes(x = start, y = length)) + 
   geom_point() + labs(x = "Starting Integers", y = "Length of Sequence")
@@ -6,7 +6,7 @@ plot1 <- ggplot(collatz_df, aes(x = start, y = length)) +
 #Top 10 starting integers are > 200 in length 
 
     
-#2 plot
+#2 plot-------------------------------------------------------------------------------
 
 #To obtain highest values in length
 collatz_highest <- filter(collatz_df, length > 200) 
@@ -18,15 +18,14 @@ top10longest
 top10 <- collatz_highest[c(16, 42, 20, 27, 40, 18, 46, 47, 23, 24), ]
 
 plot2 <- ggplot(collatz_highest, aes(x = start, y = length)) +
-  geom_point() + geom_point(data = top10, aes(x = start, y = length), colour = "red")+
+  geom_point() + 
+  geom_point(data = top10, aes(x = start, y = length), colour = "red") +
   labs(x = "Starting Integers", y = "Length of Sequence")
    
 
+#3 plot--------------------------------------------------------------------------------
 
-
-#3 plot
-
-ggplot(collatz_df) + geom_boxplot(aes())
+ggplot(collatz_df) + geom_boxplot(aes(length, parity))
 
 
 
