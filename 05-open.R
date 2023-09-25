ad2892cf86d771231d11dffde50fecf6b4168740
nicholas <21b2047@ubd.edu.bn>
2023-09-25 19:04:45 +0800

    created top_10_freq_primes
    dataframe that consists of the top 10 most frequently occurring prime numbers and their corresponding frequencies

--- 05-open.R
+++ 05-open.R
@@ -3,22 +3,6 @@
 library(tidyverse)
 library(primes)
 
-primes_df <- mutate(collatz_df, prime = is_prime(start))
-
-#prime lengths vs non prime lengths
-prime_lengths <- (filter(primes_df, prime == TRUE))$length
-non_prime_lengths <- (filter(primes_df, prime == FALSE))$length
-
-length_comparison <- data.frame(
-  group = factor(rep(c("Prime","Non-Prime"), each = 10)),
-  sequence_length = c(prime_lengths, non_prime_lengths)
-)
-
-ggplot(length_comparison, aes(x = group, y = sequence_length, fill = group)) +
-  geom_boxplot() +
-  labs(x = "Group", y = "Sequence Length") +
-  ggtitle("Comparison of Sequence Lengths for Prime and Non-Prime Starting Integers")
-
 #frequency each prime number appears in a sequence
 freq_prime <- (collatz_df$seq)[-1] %>%
   unlist() %>%
@@ -26,8 +10,10 @@ freq_prime <- (collatz_df$seq)[-1] %>%
   as.data.frame() %>%
   mutate(., prime = is_prime(.)) %>%
   filter(., prime == TRUE) %>%
-  arrange(., desc(Freq))
-colnames(freq_prime) <- c("start", "freq", "prime")
+  arrange(., desc(Freq)) %>%
+  subset(., select = -prime)
 
-ggplot(freq_prime, aes (x = 1:2432, y = freq))+
-  geom_line()
+(colnames(freq_prime) <- c("start", "freq"))
+
+top_10_freq_primes <- freq_prime[2:11,]
+rownames(top_10_freq_primes) <- c(1:10)
